b819c6435e7bd1a681275eb809d671aabf862607
Jared Khan <jaredkhan@me.com>
2016-12-09 16:56:49 +0000

    Improve application page

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { Header } from 'src/header';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App({ children }) {
   return (
     <div>
-      <header>
-          Hack Cambridge - <em>Review System</em>
-        </header>
-      <Container>
-        { children }
-      </Container>
+      <Header/>
+      { children }
     </div>
   )
 }
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -5,9 +5,10 @@ import '../css/main.css';
 
 export default function ApplicationListing() {
   return (
-    <div>
+    <Container>
       <Jumbotron>
-        <h1>Hello, Jane!</h1>
+        <h3>730 sign ups</h3>
+        <h1>354 Applications</h1>
         <p>You are the #5 reviewer, keep at it!</p>
         <Button tag={Link} to='/applications/1234' color="primary">Start Reviewing</Button>
         <Button tag={Link} to='/applications' size="large" color="success">See all Applications</Button>
@@ -22,7 +23,7 @@ export default function ApplicationListing() {
         <Col md="6">
           Leaderboard:
           <ol>
-            <li class="h4">
+            <li className="h4">
               Steve
             </li>
             <li>
@@ -37,6 +38,6 @@ export default function ApplicationListing() {
           </ol>
         </Col>
       </Row>
-    </div>
+    </Container>
   );
 }
