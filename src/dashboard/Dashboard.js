0c0c6fc87b2ae78aaa561d5f0298d9a25edfc433
varkor <github@varkor.com>
2016-12-19 18:43:45 +0000

    Made the leaderboard sync via the API

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Container, Jumbotron, Row, Col, Progress, Button } from 'reactstrap';
+import './dashboard.css';
 
 function correctPlural(amount, singular = '', plural = 's') {
-  return (amount == 1) ? singular : plural;
+  return (amount === 1) ? singular : plural;
 }
 
-export default function Dashboard({ signups, applications, reviews, userReviews, userGoal }) {
+export default function Dashboard({ signups, applications, reviews, userReviews, userGoal, leaderboard, leaderboardPosition }) {
   return (
     <Container>
       <Jumbotron>
         <h3>{signups} sign up{correctPlural(signups)}</h3>
         <h1>{applications} Application{correctPlural(applications)}</h1>
-        <p>You are the #5 reviewer, keep at it! (not working yet)</p>
+        <p>{leaderboardPosition !== null ? "You are the #" + leaderboardPosition + " reviewer, keep at it!" : "We couldn't work out your leaderboard position, sorry!"}</p>
         <Button tag={Link} to='/applications/next' color="primary">Start Reviewing</Button>{' '}
         <Button tag={Link} to='/applications' size="large" color="success">See all Applications</Button>
       </Jumbotron>
@@ -24,21 +25,8 @@ export default function Dashboard({ signups, applications, reviews, userReviews,
           <Progress value={userReviews} max={userGoal} color="warning"/>
         </Col>
         <Col md="6">
-          Leaderboard (not working yet):
-          <ol>
-            <li className="h4">
-              Steve
-            </li>
-            <li>
-              Alice
-            </li>
-            <li>
-              Joe
-            </li>
-            <li>
-              Jane
-            </li>
-          </ol>
+          Leaderboard:
+          <ol>{leaderboard.map(reviewer => <li key={"leaderboard-entry-" + reviewer.id}><span className="leaderboard-name">{reviewer.name}</span>  &ndash; <span className="leaderboard-score">{reviewer.count}</span></li>)}</ol>
         </Col>
       </Row>
     </Container>
