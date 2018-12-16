7bdf5cc05c2c0ee64c26c8b9ed0ac2fe850b0978
Henry Mercer <henry.mercer@me.com>
2018-12-16 23:51:41 +0000

    Refactor tools page rendering

--- src/tools-page/Tools.js
+++ src/tools-page/Tools.js
@@ -2,23 +2,33 @@ import React from 'react';
 import { Button, Container, Row } from 'reactstrap';
 
 class Tools extends React.Component {
+  renderCsvDownload(title, description, kind) {
+    return (
+      <Row>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Button className="btn btn-lg btn-outline-dark btn-block" onClick={ () => this.props.downloadCsv(kind) }>Download</Button>
+      </Row>
+    );
+  }
+
   render () {
     return (
       <Container>
         <Row><h2>CSVs of unfinished applications</h2></Row>
 
-        <Row>
-          <h3>Signed in but not started an application</h3>
-          <p>Applicants who have logged in via MyMLH, but have not yet submitted an individual application.</p>
-          <Button className="btn btn-lg btn-outline-dark btn-block" onClick={ () => this.props.downloadCsv('individual') }>Download</Button>
-        </Row>
+        {this.renderCsvDownload(
+          'Signed in but not started an application',
+          'Applicants who have logged in via MyMLH, but have not yet submitted an individual application.',
+          'individual'
+        )}
 
-        <Row>
-          <h3>Have yet to submit team grouping</h3>
-          <p>Applicants who have submitted an individual application and said they wanted to apply in a team, but they
-            haven't yet submitted their team grouping.</p>
-          <Button className="btn btn-lg btn-outline-dark btn-block" onClick={ () => this.props.downloadCsv('team-only') }>Download</Button>
-        </Row>
+        {this.renderCsvDownload(
+          'Have yet to submit team grouping',
+          'Applicants who have submitted an individual application and said they wanted to apply in a team, but they ' +
+          'haven’t yet submitted their team grouping.',
+          'team-only'
+        )}
       </Container>
     );
   }
